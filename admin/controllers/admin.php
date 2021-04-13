a5d13a3971a6387edfe18e94bcb1bff435c5db97
CodeSoftly <eclittlejohn@yahoo.com>
2021-04-12 21:11:13 -0500

    commit

--- admin/controllers/admin.php
+++ admin/controllers/admin.php
@@ -18,17 +18,17 @@ switch($action) {
         break;
 
     case "register":
+        $username = filter_input(INPUT_POST,'username', FILTER_SANITIZE_STRING);
+        $password = filter_input(INPUT_POST,'password',FILTER_SANITIZE_STRING);
+        $confirm_password = filter_input(INPUT_POST,'confirm_password', FILTER_SANITIZE_STRING);
         include('util/valid_register.php');
         $errors = valid_registration($username, $password, $confirm_password);
         if($errors) {
             include('view/register.php');
         } else {
-        $username = filter_input(INPUT_POST,'username');
-        $password = filter_input(INPUT_POST,'password');
-        $confirm_password = filter_input(INPUT_POST,'confirm_password');
-                add_admin($username,$password);
+                add_admin($username, $password);
                 $_SESSION['is_valid_admin'] = true;
-                header('Location: .?action=list_vehicles');
+                include('../admin/controllers/vehicles.php');
         }
         break;
 
